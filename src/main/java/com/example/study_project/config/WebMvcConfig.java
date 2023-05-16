005aa54f09feec39c9d23ea8ea1c89714f88b597
YuYoHan <zxzz8014@naver.com>
2023-05-16 22:12:19 +0900

    [Add] WebMvcConfig 추가

---
+++ src/main/java/com/example/study_project/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.example.study_project.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// 스프링 빈으로 등록
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 모든 경로에 대해
+        registry.addMapping("/**")
+                // Origin이 http://localhost:3000에 대해
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080")
+                // GET, POST, PUT, PATCH, DELETE, OPTIONS 메서드를 허용한다.
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+}
